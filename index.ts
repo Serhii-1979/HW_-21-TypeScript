371fe6ae75dfe636f257a943c8a5e30674d5f665
Serhii-1979 <krasovskyi76@gmail.com>
2024-10-13 00:53:58 +0200

    Add new file

--- index.ts
+++ index.ts
@@ -191,6 +191,8 @@ console.log(`Checking Account Balance: ${checking.balance}`);
 Затем создайте классы `Audio` и `Video`, которые наследуют `Media` и реализуют метод `play()` по-своему (например, `Audio` выводит "Playing audio", а `Video` — "Playing video").
 Создайте массив типа `Media[]`, включающий объекты `Audio` и `Video`, и вызовите метод `play()` для каждого элемента массива. */
 
+//
+
 
 abstract class Media {
     abstract play(): void;
